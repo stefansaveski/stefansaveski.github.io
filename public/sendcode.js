319077222948ebbe93d4f63f3b9f24bfe48f2367
Kiko117001 <hristijansaveski@yahoo.com>
2023-12-14 15:39:39 +0100

    v1.2

--- public/sendcode.js
+++ public/sendcode.js
@@ -1,12 +1,26 @@
+var firstName;
+var lastName;
+var indexNumber;
+function sendData() {
+    firstName = document.getElementById("firstName").value;
+    lastName = document.getElementById("lastName").value;
+    indexNumber = document.getElementById("indexNumber").value;
+
+    var userData = `${firstName}${lastName}`;
+    console.log(userData);
+}
 async function saveToJson() {
     // Get the textarea element by its ID
     var textarea = document.getElementById("exampleFormControlTextarea1");
 
     // Get the content of the textarea
     var textareaContent = textarea.value;
-
+    console.log("firstName");
     // Replace newlines with \n
     var jsonData = {
+        name: "firstName",
+        lastName: lastName,
+        index: indexNumber,
         input: textareaContent.replace(/\n/g, "\\n")
     };
 
--- server.js
+++ server.js
@@ -13,11 +13,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/save', (req, res) => {
     const jsonData = req.body;
+    const name = req.index;
 
     // Save the JSON data to a file
     fs.writeFileSync('savedData.json', JSON.stringify(jsonData, null, 2));
 
     res.send('Data saved on the server.');
+});
+
+// Serve client.html directly
+app.get('/', (req, res) => {
+    const filePath = path.join(__dirname, 'public', 'client.html');
+    res.sendFile(filePath);
 });
 
 app.listen(port, () => {
